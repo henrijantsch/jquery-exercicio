99fc0f4df621cfb4a16317f648d9dd0ce9ab12be
Henri Jantsch <henrijantsch@hotmail.com>
2023-10-02 11:56:34 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('form').on('submit', function(e) {
+        
+        e.preventDefault();
+        
+        const novaTarefa = $('#nova-tarefa').val();
+        const novoItem = $('<li></li>');
+        $(`<li>${novaTarefa}</li>`).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $('ul').on('click', 'li', function () {
+            $(this).addClass("riscado");
+        });
+        $('#nova-tarefa').val("");
+    })
+})
+
